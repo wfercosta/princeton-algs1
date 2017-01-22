3902c50f2321a2241fb33adcc9938af8326b76bf
Wander Costa <wander@ciandt.com>
2017-01-22 15:02:33 -0200

    Adds common methods implementation

--- src/Algorithms/UnionFind/UnionFindAbstract.php
+++ src/Algorithms/UnionFind/UnionFindAbstract.php
@@ -2,6 +2,8 @@
 
 namespace Algorithms\UnionFind;
 
+use Utils\Generator;
+
 /**
  * Created by PhpStorm.
  * User: wfer
@@ -11,13 +13,43 @@ namespace Algorithms\UnionFind;
 abstract class UnionFindAbstract
 {
 
+    protected $ids = array();
+    protected $count;
+
     /**
      * UnionFindAbstract constructor.
      * @param int $n
      *  Number of site to initialize.
      */
     public function __construct(int $n) {
+        $this->count = $n;
+        foreach (Generator::range(0, $n) as $value) {
+            $this->ids[$value] = $value;
+        }
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    function __toString()
+    {
+        $keys = "| ";
+        $values = "| ";
+        foreach ($this->ids as  $key => $value) {
+            $keys .= str_pad($key, 4, '0', STR_PAD_LEFT) . " | ";
+            $values .= str_pad($value, 4, '0', STR_PAD_LEFT) . " | ";
+        }
+        echo 'Site       : ' . $keys . "\n";
+        echo 'Connection : ' . $values . "\n";
+    }
+
+    /**
+     * Returns the number of sites in the vector.
+     * @return int
+     *  The number of the
+     */
+    public function count() : int {
+        return $this->count;
     }
 
     /**
@@ -54,7 +86,6 @@ abstract class UnionFindAbstract
      * @return bool
      *  Returns true if they are connected.
      */
-    public abstract function connected(int $p, int $q): boolean;
-
+    public abstract function connected(int $p, int $q): bool;
 
 }
